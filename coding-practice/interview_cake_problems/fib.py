e7039ca5e3d95e951b5dbe56d27fc552e1ba7ef6
David Ogor <david.ogor@brightbytes.net>
2017-05-22 07:40:11 -0700

    Use memoize to improve speed of fib(n)

--- coding-practice/interview_cake_problems/fib.py
+++ coding-practice/interview_cake_problems/fib.py
@@ -1,13 +1,18 @@
 """
 Problem:  Write a function fib() that a takes an integer nnn and returns the nnnth fibonacci number.
 """
+memo = {}
 def fib(n):
     if n <= 0:
         return 0
     if n == 1:
         return 1
     else:
-        return fib(n - 2) + fib(n -1)
+        if memo.get(n -1) is None:
+            memo[n - 1] = fib(n - 1)
+        if memo.get(n -2) is None:
+            memo[n - 2] = fib(n - 2)
+        return memo[n - 2] + memo[n -1]
 
 def TestSuite():
     print("fib({0}) => {1}".format(0, fib(0)))
@@ -24,5 +29,9 @@ def TestSuite():
 
     print("fib({0}) => {1}".format(4, fib(4)))
     assert(fib(4)==  3)
+
+
+    print("fib({0}) => {1}".format(113, fib(113)))
+    assert(fib(113) ==  184551825793033096366333)
 
 TestSuite()
