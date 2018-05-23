177574ddd3fc9be3cc6ade3a4fcf13b09610fbb1
David Ogor <david.ogor@brightbytes.net>
2018-05-23 05:46:50 -0700

    Completed

--- coding-practice/daily_coding_problem/18_max.py
+++ coding-practice/daily_coding_problem/18_max.py
@@ -10,3 +10,40 @@
 
     Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store the results. You can simply print them out as you compute them.
 """
+from queue import PriorityQueue
+
+
+def find_max(arr, k):
+    result = []
+    heap = PriorityQueue()
+
+    # insert k - 1 elements into heap
+    for i in range(min(len(arr), k - 1)):
+        heap.put((-arr[i], i))
+
+    # add value and get max
+    for i in range(k - 1, len(arr)):
+        heap.put((-arr[i], i))
+        found = False
+        while not found:
+            max_val, idx = heap.get()
+            if (i - idx) < k:
+                result.append(-max_val)
+                heap.put((max_val, idx))
+                found = True
+
+    return result
+
+
+def TestSuite():
+    nums = [10, 5, 2, 7, 8, 7]
+    k = 3
+    expected = [10, 7, 8, 8]
+    actual = find_max(nums, k)
+
+    assert expected == actual, "{} == {}".format(expected, actual)
+
+    print("Successfully completed")
+
+if __name__ == "__main__":
+    TestSuite()
