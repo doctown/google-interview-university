14e1bdf74f2aaf700f7e94a19b1971b31e72a8ec
David Ogor <david.ogor@brightbytes.net>
2018-06-02 09:24:01 -0700

    Completed 31

--- coding-practice/daily_coding_problem/31_edit_distance.py
+++ coding-practice/daily_coding_problem/31_edit_distance.py
@@ -4,3 +4,58 @@ The edit distance between two strings refers to the minimum number of character 
 Given two strings, compute the edit distance between them.
 """
 
+
+def edit_distance(first_word, second_word):
+    T = [[0] * (len(second_word) + 1) for _ in range(len(first_word) + 1)]
+    for i in range(len(T)):
+        T[i][0] = i
+
+    for i in range(len(T[0])):
+        T[0][i] = i
+
+    T[0][0] = 0
+
+    for first_word_index in range(1, len(T)):
+        for second_word_index in range(1, len(T[0])):
+            # if letters the same then pre of both
+            T[first_word_index][second_word_index] = T[first_word_index - 1][second_word_index - 1] if first_word[first_word_index - 1] == second_word[second_word_index - 1] else min(T[first_word_index][second_word_index - 1], T[first_word_index - 1][second_word_index], T[first_word_index - 1][second_word_index - 1]) + 1
+
+    return T[-1][-1]
+
+
+def TestSuite():
+    first_string = "kitten"
+    second_string = "kitten"
+    expected = 0
+    actual = edit_distance(first_string, second_string)
+
+    assert expected == actual, "{} == {}".format(expected, actual)
+
+    first_string = "kitten"
+    second_string = "kite"
+    expected = 2
+    actual = edit_distance(first_string, second_string)
+
+    assert expected == actual, "{} == {}".format(expected, actual)
+
+    first_string = "sit"
+    second_string = "sitting"
+    expected = 4
+    actual = edit_distance(first_string, second_string)
+
+    assert expected == actual, "{} == {}".format(expected, actual)
+
+    first_string = "kitten"
+    second_string = "sitting"
+    expected = 3
+    actual = edit_distance(first_string, second_string)
+
+    assert expected == actual, "{} == {}".format(expected, actual)
+
+    print("Done")
+
+
+TestSuite()
+
+
+
